a4d0ef0751078ec7641aaf49fba70e800522253c
troyjr <troyjrose@gmail.com>
2014-05-25 08:03:35 +1000

    Implemented matrix solving code.
    Comments, code and examples.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## Set of functions which create a matrix cacheing object to cache results of
+## inverting a matrix - solve(x) for computationally long matrix operations
+##
+## Usage:
+## > # Create square matrix that you want to solve
+## > n = 4
+## > squarematrix <- matrix(rnorm(n*n), n, n)
+## > # create caching matrix object
+## > x <- makeCacheMatrix(squarematrix)
+## > # calculate inverted matrix
+## > View(cacheSolve(x))
+## > # running again, it fetches results from cache
+## > View(cacheSolve(x))
+## getting cached data
+## >
 
-## Write a short comment describing this function
+## Sets up caching object.
+## first argument is square matrix for caching operations
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        
+        list(set = set, get = get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## Solves and caches square matrix inversion
+## subsequent runs are cached
+## first argument is object as initialised from 'makeCacheMatrix' function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
